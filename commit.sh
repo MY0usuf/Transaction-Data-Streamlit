dcbc66e4639e09f6d192542935810be5b3168eea
MY0usuf <muhammady.be22@uceou.edu>
2023-06-26 14:02:22 +0400

    was testing

--- commit.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-cd C:\\Users\\yousu\\Documents\\GitHub\\Transaction-Data-Streamlit
-git add --all
-timestamp() {
-  date +"at %H:%M:%S on %d/%m/%Y"
-}
-git commit -am "Regular auto-commit $(timestamp)"
-git push origin master
