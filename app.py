b14b0ee3a466d9760c6431558b9e1e60e179f909
MY0usuf <muhammady.be22@uceou.edu>
2023-06-26 13:51:19 +0400

    Regular auto-commit at 13:51:19 on 26/06/2023

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import pandas as pd
 import numpy as np
 import os
 import datetime
-from streamlit_autorefresh import st_autorefresh
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
