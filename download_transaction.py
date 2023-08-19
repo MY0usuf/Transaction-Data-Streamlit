14076ab768fdb9b18f97a59b9a8b1c812841d054
MY0usuf <muhammady.be22@uceou.edu>
2023-08-19 14:58:20 +0400

    autoCommit 20230819.145819

--- download_transaction.py
+++ download_transaction.py
@@ -40,7 +40,7 @@ def download_transaction(base_url,download_dir,date):
     #service = Service(executable_path=PATH)
     #service = Service(ChromeDriverManager().install())
     service = Service()
-    #service = Service(ChromeDriverManager().install())
+    service = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service, options=options) # Initialising the driver by giving out the PATH to chromedriver.exe
     
     # Getting Todays Date and Month to use while filling out the form
